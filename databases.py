217775dc79cc741a60ed9417d7016b1c75adc40f
aboud20-meet <aboud20@meet.mit.edu>
2018-11-01 18:43:55 +0200

    basic databases

--- databases.py
+++ databases.py
@@ -10,16 +10,33 @@ session = DBSession()
 
 # Write your functions to interact with the database here :
 
-def create_product():
+def create_product(name, price, in_stock, rank):
   #TODO: complete the functions (you will need to change the function's inputs)
-  pass
+  product_object = Product(
+      name=name,
+      price=price,
+      in_stock=in_stock,
+      rank= rank)
+  session.add(product_object)
+  session.commit()
 
-def update_product():
+
+def update_product(name ,Nname, Nprice):
   #TODO: complete the functions (you will need to change the function's inputs)
-  pass
+  if Nprice<=300:
+  	product_object = session.query(Product).filter_by(name = name).first()
+  	product_object.name = Nname
+  	product_object.price = Nprice
+  	session.commit()
+  else:
+  	print("Too bad!! the price is not in the range")
+
 
 def delete_product(id):
-  pass
+  session.query(Product).filter_by(
+      id=id).delete()
+  session.commit()
+
 
 def get_product(id):
   pass
